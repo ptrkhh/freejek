57c46258bd7ae9d73469613c632b8e0b8fef598a
Patrick Hermawan <patrick.hermawan@outlook.com>
2025-04-11 02:58:13 +0700

    todo todo todo todo todo todo todooooo todo todo

---
+++ frontend/pages/driver_trip_new.py
@@ -0,0 +1,7 @@
+
+# TODO POLL nearby AVAILABLE riders with their locations
+# TODO Show on map
+# TODO show table of nearby available: type, fare, distance, passengers
+# TODO click on item = show direction on map GET /route/v1/{profile}/{coordinates}?alternatives={true|false}&steps={true|false}&geometries={polyline|polyline6|geojson}&overview={full|simplified|false}&annotations={true|false}
+# TODO button to ACCEPT ride (incompatible = no accept button)
+# TODO redirect to driver trip current
--- frontend/pages/rider_trip_new.py
+++ frontend/pages/rider_trip_new.py
@@ -1,12 +1,17 @@
-import folium
-import streamlit as st
-from streamlit_folium import st_folium
 
-center_lat, center_lon = 39.949610, -75.150282
+# TODO textarea to search POI
+# TODO search button GET http://{server}/nearest/v1/{profile}/{coordinates}.json?number={number}
+# TODO show map in center
+# TODO tap on map = move dot
+# TODO button --> Set as pickup point
+# TODO button --> Set as destination
+# TODO radio --> vehicle type (car/bike)
+# TODO radio --> vehicle class
+# TODO slider --> passengers (when bike = disabled)
 
-if "random_lat" not in st.session_state or "random_lon" not in st.session_state:
-    st.session_state.random_lat = center_lat
-    st.session_state.random_lon = center_lon
-m = folium.Map(location=[center_lat, center_lon], zoom_start=16)
-folium.CircleMarker([st.session_state.random_lat, st.session_state.random_lon]).add_to(m)
-st_folium(m, width=725)
+# TODO when (pickup or dstination updated) and (pickup and destination) hit endpoint GET /route/v1/{profile}/{coordinates}?alternatives={true|false}&steps={true|false}&geometries={polyline|polyline6|geojson}&overview={full|simplified|false}&annotations={true|false}
+# TODO and redraw
+# TODO and provide fare calculation
+
+# TODO textarea to NOTES FROM RIDER
+# TODO ORDER RIDE
