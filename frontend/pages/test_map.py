1a5c608d7e2bd0f05bf01ae7244ac16ce37d5754
Patrick Hermawan <patrick.hermawan@outlook.com>
2025-04-13 01:10:55 +0700

    bisa klik

--- frontend/pages/test_map.py
+++ frontend/pages/test_map.py
@@ -11,28 +11,33 @@ res = get_geolocation()
 while not res:
     time.sleep(1)
 
-st.session_state["lat"], st.session_state["lon"] = res["coords"]["latitude"], res["coords"]["longitude"]
-# st.session_state["lat"], st.session_state["lon"]=39.949610, -75.150282
-st.session_state["initial_lat"], st.session_state["initial_lon"] = st.session_state["lat"], st.session_state["lon"]
-last_count = 0
-count = st_autorefresh(interval=5000, key="fizzbuzzcounter")
+if not ("lat" in st.session_state and "lon" in st.session_state):
+    st.session_state["lat"], st.session_state["lon"] = res["coords"]["latitude"], res["coords"]["longitude"]
+if not ("initial_lat" in st.session_state and "initial_lon" in st.session_state):
+    st.session_state["initial_lat"], st.session_state["initial_lon"] = st.session_state["lat"], st.session_state["lon"]
+if not ("last_clicked_lat" in st.session_state and "last_clicked_lon" in st.session_state):
+    st.session_state["last_clicked_lat"], st.session_state["last_clicked_lon"] = 0.0, 0.0
+if "last_count" not in st.session_state:
+    st.session_state["last_count"] = 0
+count = st_autorefresh(interval=1000, key="fizzbuzzcounter")
 
 
 def update_position():
     st.session_state["lat"] = st.session_state["lat"] + random.uniform(-0.001, 0.001)
     st.session_state["lon"] = st.session_state["lon"] + random.uniform(-0.001, 0.001)
-    # random_lat = center_lat + random.uniform(-0.001, 0.001)
-    # random_lon = center_lon + random.uniform(-0.001, 0.001)
     return folium.RegularPolygonMarker([st.session_state["lat"], st.session_state["lon"]])
 
-def update_position2():
-    return folium.CircleMarker([st.session_state["last_clicked"]["lat"], st.session_state["last_clicked"]["lng"]])
+def update_position2(lat, lon):
+    print("ADDING CIRCLE MARKER AT", lat, lon)
+    return folium.CircleMarker([lat, lon])
 
 
 
 fg = folium.FeatureGroup(name="Moving Marker")
 fg2 =  folium.FeatureGroup(name="Clicking Marker")
 fg.add_child(update_position())
+print("LAST CLICKED", st.session_state["last_clicked_lat"], st.session_state["last_clicked_lon"])
+fg2.add_child(update_position2(st.session_state["last_clicked_lat"], st.session_state["last_clicked_lon"]))
 out = st_folium(
     folium.Map(location=[st.session_state["initial_lat"], st.session_state["initial_lon"]], zoom_start=16),
     feature_group_to_add=[fg, fg2],
@@ -43,14 +48,12 @@ out = st_folium(
 print("THE OUT", out)
 
 if "last_clicked" in out and out["last_clicked"]:
-    st.session_state["last_clicked"] = out["last_clicked"]
-    last =  st.session_state["last_clicked"]
-    if "lat" in last and last["lat"] and "lng" in last and last["lng"]:
-        fg2.add_child(update_position2())
+    print('THE LAST CLICKED1', out["last_clicked"])
+    st.session_state["last_clicked_lat"] = out["last_clicked"]["lat"]
+    st.session_state["last_clicked_lon"] = out["last_clicked"]["lng"]
 
 st.text_input(label="something")
 
-if count != last_count:
+if count != st.session_state["last_count"]:
     last_count = count
-    fg.add_child(update_position())
-    fg2.add_child(update_position2())
+
